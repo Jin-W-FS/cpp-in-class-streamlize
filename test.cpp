82fa51f022d491cffabd438355bfe1db5abf2882
Jin.W.FS <Jin.W.FSeer@gmail.com>
2014-02-27 11:04:56 +0800

    complete test case;
    proved that before c++0x a const referance is always needed if u want to output a temp object(rvalue reference).

--- test.cpp
+++ test.cpp
@@ -2,21 +2,57 @@
 
 #include "streamlize.hpp"
 
-struct A
-{
+struct A {
 	int x;
-	A(int _x):x(_x) {
-	}
-	__to_stream__(std::ostream& os) const {
-		os << x;
-	}
+	A(int _x):x(_x) {}
+	__to_stream__(std::ostream& os) const { os << x; }
 };
+
+struct B {
+	int x;
+	B(int _x):x(_x) {}
+	__to_stream__(std::ostream& os) { os << x; }
+};
+
+struct C {
+	int x;
+	C(int _x):x(_x) {}
+};
+std::ostream& operator<<(std::ostream& os, const C& obj) {
+	return os << obj.x;
+}
+
+struct D {
+	int x;
+	D(int _x):x(_x) {}
+};
+std::ostream& operator<<(std::ostream& os, D& obj) {
+	return os << obj.x;
+}
+#ifdef __GXX_EXPERIMENTAL_CXX0X__
+std::ostream& operator<<(std::ostream& os, D&& obj) {
+	return os << obj.x;
+}
+#endif
 
 int main()
 {
-	A a(3);
-	std::cout << "A(3): " << a << std::endl;
-	std::cout << "A(2): " << A(2) << std::endl;
+	A a(0);
+	std::cout << "A(0): " << a << std::endl;
+	std::cout << "A(1): " << A(1) << std::endl;
+	B b(0);
+	std::cout << "B(0): " << b << std::endl;
+#ifdef __GXX_EXPERIMENTAL_CXX0X__
+	std::cout << "B(1): " << B(1) << std::endl;	// only after c++0x
+#endif
+	C c(0);
+	std::cout << "C(0): " << c << std::endl;
+	std::cout << "C(1): " << C(1) << std::endl;
+	D d(0);
+	std::cout << "D(0): " << d << std::endl;
+#ifdef __GXX_EXPERIMENTAL_CXX0X__
+	std::cout << "D(1): " << D(1) << std::endl;	// before c++0x, rvalue ref is treated as const ref
+#endif
 	return 0;
 }
 
