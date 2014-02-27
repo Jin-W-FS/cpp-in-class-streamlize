063a8c204eec5555259718426f4d785bfc970a3b
Jin.W.FS <Jin.W.FSeer@gmail.com>
2014-02-27 09:25:10 +0800

    also fix in -std=c++98; problem: type cast discard const qualifier in streamlizer::streamlize_fn

--- streamlize.hpp
+++ streamlize.hpp
@@ -25,19 +25,19 @@ std::ostream& operator<<(std::ostream& os, T&& obj) {
 class __streamlize_helper {
 private:
 	struct streamlizer_base {
-		void* p;
-		void (*fn)(std::ostream& os, void* p);
-		streamlizer_base(void* p_, void (*fn_)(std::ostream&, void*)):p(p_), fn(fn_) {}
+		const void* p;
+		void (*fn)(std::ostream& os, const void* p);
+		streamlizer_base(const void* p_, void (*fn_)(std::ostream&, const void*)):p(p_), fn(fn_) {}
 		void __to_stream(std::ostream& os) const { fn(os, p); }
 	};
 	template <class T, class = typename T::streamlizable>
 	struct streamlizer {
-		static void streamlize_fn(std::ostream& os, void* p) { ((T*)p)->__to_stream__(os); }
+		static void streamlize_fn(std::ostream& os, const void* p) { ((T*)p)->__to_stream__(os); }
 	};
 private:
 	streamlizer_base s;
 public:
-	template <class T> __streamlize_helper(T& obj_):s(&obj_, streamlizer<T>::streamlize_fn) {}
+	template <class T> __streamlize_helper(const T& obj_):s(&obj_, streamlizer<T>::streamlize_fn) {}
 	void __to_stream(std::ostream& os) const { s.__to_stream(os); }
 };
 
